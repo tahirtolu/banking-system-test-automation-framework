906ea49a24c2134ba6a29ba460021cffbc319add
tahirtolu <99593620+tahirtolu@users.noreply.github.com>
2026-01-04 05:27:14 +0300

    update

--- selenium-tests/src/test/java/com/banking/selenium/BaseSeleniumTest.java
+++ selenium-tests/src/test/java/com/banking/selenium/BaseSeleniumTest.java
@@ -6,25 +6,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.logging.Level;
 
 public class BaseSeleniumTest {
     protected WebDriver driver;
     protected WebDriverWait wait;
     protected static final String BASE_URL = "http://localhost:8081";
-    // Frontend URL - Docker container'dan servis edilen frontend
     protected static final String FRONTEND_URL = "http://localhost:8082";
 
     @BeforeEach
     public void setUp() {
         WebDriverManager.chromedriver().setup();
+
+        // Chrome options
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
+
+        // Console loglarını yakala
+        LoggingPreferences logPrefs = new LoggingPreferences();
+        logPrefs.enable(LogType.BROWSER, Level.ALL);
+        options.setCapability("goog:loggingPrefs", logPrefs);
+
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -38,4 +48,3 @@ public class BaseSeleniumTest {
         }
     }
 }
-
